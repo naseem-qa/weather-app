3c65e5e234ba7d1b28558625052dadfe406da007
naseem <izzatnaseem30@gmail.com>
2020-06-11 23:14:46 +0300

    some change

--- src/components/main/location.js
+++ src/components/main/location.js
@@ -3,16 +3,29 @@ import useFetch from '../hooks/useFetch.js';
 
 function Location (props){
   const [location, setLocation] = useState({});
-  useFetch('http://localhost:3030/location',{},setLocation);
-  console.log('__STATE__', location);
+  // useFetch('http://localhost:3030/location',{},setLocation);
+  
+  // console.log('__STATE__', location);
+  
+  const gitData = (cityName)=>{
 
+    fetch('http://localhost:3030/location')
+      .then(data => {
+        data.json()
+          .then(data => setLocation(data));
+      });
+  };
   const showState = () => {
+  // .then(data => console.log('tessst', data));
     console.log('__STATE__ 2', location);
   };
 
   return(
     <>
       <h3 onClick={showState}>********** </h3>
+      <form>
+        <input type='radial' value='seattle' />
+      </form>
     </>
   );
 }
